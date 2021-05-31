757e695ef9f54bb17e67ca065b6527d10a024f8f
Ted Keim <tedkeim@yahoo.com>
2021-05-30 21:08:54 -0400

    added inquirer, started index.js questions

---
+++ develop/index.js
@@ -0,0 +1,66 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+// add generateMarkdown.js
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+    type: 'input',
+    name: '',
+    message: '',
+},
+
+{
+
+}];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
